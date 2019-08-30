6feaeb54b90d81b4f6ae8ab1cdb24191f9879d99
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-30 20:03:09 +0530

    Create Scanner object and and take user input

--- src/Solution03/Solution03.java
+++ src/Solution03/Solution03.java
@@ -1,10 +1,13 @@
 package Solution03;
 
+import java.util.Scanner;
+
 public class Solution03 {
     public static void main(String[] args) {
         double fahrenheit;
         double celsius;
-
+        Scanner scan = new Scanner(System.in);
+        fahrenheit = scan.nextDouble();
 
     }
 
