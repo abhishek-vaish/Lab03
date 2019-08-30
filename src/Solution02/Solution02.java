11e62f10b8910596e206a31c93a145434ee9396c
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-30 19:56:34 +0530

    Create Scanner object and take user input.

--- src/Solution02/Solution02.java
+++ src/Solution02/Solution02.java
@@ -1,10 +1,14 @@
 package Solution02;
 
+import java.util.Scanner;
+
 public class Solution02 {
     public static void main(String[] args) {
         double Rupay = 0.0;
         int rupay1;
         int paise;
+        Scanner scan = new Scanner(System.in);
+        Rupay = scan.nextDouble();
 
     }
 
